2e6d446e018598615fd4d992e3ac2a4120a9b9dc
Phillip Lange <phillip@lange-privat.net>
2020-12-07 16:42:32 +0100

    Chance berechnen und Produkt hinzufügen

--- Datensatzerstellung.py
+++ Datensatzerstellung.py
@@ -91,15 +91,15 @@ product_chance_marital = {"ledig": [0, 0, 0, 10, 0, 0, -10], \
                             "verheiratet": [10, 30, 0, -20, 5, 15, 15], \
                             "aufgelöste Beziehung": [-5, 0, 5, 0 , 0, 0, -15]}
 
-product_chance_age = {"very low": [0, 20, -20, -40, -5, -5, -20], \
-                        "low": [-5, 10, -10, -30, 70, 65, 85], \
-                        "low mid": [30, 50, 30, 10, 70, 60, 40], \
-                        "high mid": [10, 35, 20, 5, 5, 80, 5], \
-                        "high": [60, 50, 5, 40, 70, 65, 85], \
-                        "very high": [60, 50, 5, 40, 70, 65, 85]}
+product_chance_income = {"very low": [0, 20, -20, -40, -5, -5, -20], \
+                        "low": [-5, 10, -10, -30, -10, -10, -5], \
+                        "low mid": [0, 10, 0, -15, 0, 0, 5], \
+                        "high mid": [10, 35, 20, 5, 5, 15, 15], \
+                        "high": [0, -10, 10, 40, 10, 25, 10], \
+                        "very high": [-10, -30, 10, 20, 5, 15, 0]}
 
-product_chance_child = {True: [-5, 10, 0, 20, -5, -15, 15], \
-                        False: [5, 0, -5, -20, 20, 30, 10]}
+product_chance_child = {"ja": [-5, 10, 0, 20, -5, -15, 15], \
+                        "nein": [5, 0, -5, -20, 20, 30, 10]}
 
 ges = list()
 
@@ -152,31 +152,34 @@ for i in range(100000):
         distro = randint(1,100)
         for i in range(len(marital_prozente)):
             if distro <= marital_prozente[i]:
-                marital = marital_list[i]
-                eintrag.append(marital)
+                marital_ident = marital_list[i]
                 break
 
     else:
-        eintrag.append(choice(marital_list))
+        marital_ident = choice(marital_list)
+
+    eintrag.append(marital_ident)
 
 #Generation Kind-Merkmal
     if geschlecht_ident != "d":
         child_query = geschlecht_ident + "_child_" + alter_ident
         child_prozente = child_dic.get(child_query)
         
-        if marital == "ledig":
+        if marital_ident == "ledig":
             child_prozente[0] -= 15
 
         distro = randint(1,100)
         for i in range(len(child_prozente)):
             if distro <= child_prozente[i]:
-                child = child_list[i]
-                eintrag.append(child)
+                child_ident = child_list[i]
+                
                 kinderbonus = 5000
                 break
             
     else:
-        eintrag.append(choice(child_list))
+        child_ident = choice(child_list)
+            
+    eintrag.append(child_ident)
     
     #Generation Gehalt
     Zufallsfaktor = uniform(0.7, 1.3)
@@ -187,7 +190,7 @@ for i in range(100000):
     for i in income_list:
         if int(income) in range(i[0], i[1]):
             income = round(randint(i[0], i[1]), -3)
-            gehalt = i[2]
+            gehalt_ident = i[2]
             break
     
     eintrag.append(income)
@@ -195,9 +198,21 @@ for i in range(100000):
     produkt = choice(product_list)
     eintrag.append(produkt)
 
-    gekauft = True
-    eintrag.append(gekauft)
+    produkt_index = product_list.index(produkt)
 
+    chance = product_chance_age.get(alter_ident)[produkt_index] + product_chance_child.get(child_ident)[produkt_index] + \
+            product_chance_gender.get(geschlecht_ident)[produkt_index] + product_chance_income.get(gehalt_ident)[produkt_index] + \
+            product_chance_marital.get(marital_ident)[produkt_index] 
+
+    if chance >= 100:
+        chance -= 3
+    
+    distro = randint(0,100)
+    if distro <= chance:
+        gekauft = "ja"
+    else:
+        gekauft = "nein"
+    eintrag.append(gekauft)
 
     ges.append(eintrag)
 #endregion

6915e95f33dca640f90cf674bd8b8c4912b1208e
Phillip Lange <phillip@lange-privat.net>
2020-12-13 11:54:01 +0100

    Änderung Basisdatei mit Wikibeispiel

--- app.py
+++ app.py
@@ -1,22 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
 import dash
+import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_scripts="Stylesheet.css")
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout=html.Div(children=[
-    html.Div(className="grid-container",
-    children=[
-        html.Div(className="Tab1",
-            children=[html.P("Dashboard")]),
-        html.Div(className="Tab2",
-            children=[html.P("Vorhersagealgorithmus")])
-    ])
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
 ])
 
-#@app.route("/")
-#def main():
-#    return "Welcome!"
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)

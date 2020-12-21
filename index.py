2d7534c5a8f0f6b405fa191f39db9077c5df189f
Phillip Lange <phillip@lange-privat.net>
2020-12-21 10:19:02 +0100

    Anpassung Dateinamen

--- index.py
+++ index.py
@@ -4,18 +4,18 @@ from dash.dependencies import Input, Output
 from app import app 
 from app import server 
 
-from apps import dashboard
-from apps import dashboard2
-from apps import dashboard3
+from apps import dashboard_kpi
+from apps import dashboard_zeit
+from apps import dashboard_bcg
 from apps import tool 
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div([
         dcc.Link('Tool|', href='/apps/tool'),
-        dcc.Link('Dashboard', href='/apps/dashboard'),
-        dcc.Link('Dashboard2', href='/apps/dashboard2'),
-        dcc.Link('Dashboard3', href='/apps/dashboard3')
+        dcc.Link('Dashboard', href='/apps/dashboard_kpi'),
+        dcc.Link('Dashboard2', href='/apps/dashboard_zeit'),
+        dcc.Link('Dashboard3', href='/apps/dashboard_bcg')
     ], className="row"),
     html.Div(id='page-content', children=[])
 ])
@@ -25,12 +25,12 @@ app.layout = html.Div([
 def display_page(pathname):
     if pathname == '/apps/tool':
         return tool.layout
-    if pathname == '/apps/dashboard':
-        return dashboard.layout
-    if pathname == '/apps/dashboard2':
-        return dashboard2.layout
-    if pathname == '/apps/dashboard3':
-        return dashboard3.layout
+    if pathname == '/apps/dashboard_kpi':
+        return dashboard_kpi.layout
+    if pathname == '/apps/dashboard_zeit':
+        return dashboard_zeit.layout
+    if pathname == '/apps/dashboard_bcg':
+        return dashboard_bcg.layout
     else:
         return tool.layout
 

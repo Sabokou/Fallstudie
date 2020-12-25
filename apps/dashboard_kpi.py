e8482086c9302999b00cc1ab9417fe974def412d
Sabokou <52411026+Sabokou@users.noreply.github.com>
2020-12-25 20:38:56 +0100

    app import hinzugefügt

--- apps/dashboard_kpi.py
+++ apps/dashboard_kpi.py
@@ -2,6 +2,7 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from app import app
 
 import pandas as pd
 import dask.dataframe as dd

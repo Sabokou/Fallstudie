447af923fe2cb512adf6b8a9f751e5bd2b705b35
Sabokou <52411026+Sabokou@users.noreply.github.com>
2020-12-28 14:28:44 +0100

    Prep for dashboard caching

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#import for caching
+# import os
+# from flask_caching import Cache
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}])
 
 server = app.server 
+
+# cache = Cache(app.server, config={
+#     'CACHE_TYPE': 'filesystem',
+# })
 

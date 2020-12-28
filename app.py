96f31e956387fcea3bcee04043620e48bcf53ac3
Sabokou <52411026+Sabokou@users.noreply.github.com>
2020-12-28 16:42:23 +0100

    Implemented Cache

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 #import for caching
-# import os
-# from flask_caching import Cache
+import os
+from flask_caching import Cache
 
 app = dash.Dash(__name__, suppress_callback_exceptions=True, meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}])
 
 server = app.server 
 
-# cache = Cache(app.server, config={
-#     'CACHE_TYPE': 'filesystem',
-# })
+cache = Cache(server, config={
+     'CACHE_TYPE': 'simple'})
 

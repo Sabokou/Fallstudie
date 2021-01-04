bc9e8ad8521b8d8b996911c5673a66dd48783d9c
kiljoss <kilian.ebi@kybeidos.de>
2021-01-04 14:34:28 +0100

    app title and icon

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app = dash.Dash(__name__, suppress_callback_exceptions=True,\
       meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],\
       external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+app.title = 'Plutos Consulting'
+
+
 server = app.server 
 
 cache = Cache(server, config={

847b3c3534363177cca07cc80e748aefe6f6a2fc
Phillip Lange <phillip@lange-privat.net>
2021-01-05 09:13:55 +0100

    update title changed

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from flask_caching import Cache
 app = dash.Dash(__name__, suppress_callback_exceptions=True,\
       meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}],\
       external_stylesheets=[dbc.themes.BOOTSTRAP],\
-      title= "Dashboard")
+      title= "Dashboard",
+      update_title='Berechne...')
 
 server = app.server 
 
